1ddbc6392f05417c53bd31ebf1951f5d19fc7e7c
jowkemm <pimkote1615@gmail.com>
2026-01-05 13:35:58 +0700

    feat: add ProfileCard component and pass via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+// src/App.jsx
+import ProfileCard from './components/ProfileCard';
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>My React App</h1>
-       </div>
-    </>
-  )
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>My Team Portfolio</h1>
+
+      {/* ลองส่ง Props ข้อมูลของตัวเองลงไป */}
+      <ProfileCard 
+        name="ปาณัสม์ พิมพ์โคตร" 
+        role="Student @ CEDT" 
+        bio="ไม่หล่อให้สาวเหลียว กินก๋วยเตี๋ยวให้สาวเลี้ยง" 
+      />
+
+      {/* ลองเรียกใช้ซ้ำอีกครั้งด้วยข้อมูลที่ต่างออกไป */}
+      <ProfileCard 
+        name="John Doe" 
+        role="Guest Developer" 
+        bio="I love coding and learning new things." 
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
